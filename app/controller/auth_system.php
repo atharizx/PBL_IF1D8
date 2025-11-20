1b84e29cce8aaccd09dbea2dcb462fd96ef1f227
sena <atharizkysena@gmail.com>
2025-11-20 21:49:25 +0700

    Update Login system

--- app/controller/auth_system.php
+++ app/controller/auth_system.php
@@ -1,27 +1,37 @@
 <?php
-include 'db_conn.php';
+include '../config/db_conn.php';
 session_start();
 
 $nidn = $_POST['nidn'];
 $password = $_POST['pass'];
 
-$sql = "SELECT * FROM adminlog WHERE nidn = '$nidn' AND pw = '$password'";
-$result = mysqli_query($conn, $sql);
+$stmt = $conn->prepare("SELECT nidn, pw, nama FROM adminlog WHERE nidn = ?");
+$stmt->bind_param("s", $nidn);
+$stmt->execute();
+$stmt->store_result();
 
-if (mysqli_num_rows($result) === 1) {
-    $row = mysqli_fetch_assoc($result);
+if ($stmt->num_rows === 1) {
+    $stmt->bind_result($db_nidn, $db_pw, $db_nama); 
+    $stmt->fetch();
 
-    $_SESSION['nidn'] = $row['nidn'];
-    $_SESSION['name'] = $row['nama'];
+    if ($password === $db_pw) {
+        $_SESSION['nidn'] = $db_nidn;
+        $_SESSION['nama'] = $db_nama;
 
-    $_SESSION['login_success'] = "Selamat datang " . $row['nama'];
+        $_SESSION['login_success'] = "Selamat Datang " . $_SESSION['nama'] ."!";
 
-    header("Location: ../public/admin/penerbitan.php");
-    exit();
-} else {
-    $_SESSION['login_error'] = "NIDN atau Password salah.";
-    header("Location: ../public/admin/loginpage.php");
-    exit();
+        header("Location: ../../public/admin/penerbitan.php");
+        exit();
+    } else {
+        $_SESSION['login_error'] = "NIDN atau Password salah.";
+        header("Location: ../../public/admin/loginpage.php");
+        exit();
+    }
+
 }
+$stmt -> close();
+$conn -> close();
 
 ?>
+   
+
