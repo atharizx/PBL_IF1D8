4086b0581f3e6fce0c2b01278ae7070943eb0d3a
sena <atharizkysena@gmail.com>
2025-11-30 22:03:44 +0700

    Update auth_models.php untuk komunikasi ke database pada file auth_controller.php

--- app/models/auth_model.php
+++ app/models/auth_model.php
@@ -1,6 +1,4 @@
 <?php
-require_once '../config/db_conn.php';
-require_once '../controller/auth_controller.php';
 
 function getAdminByNidn($conn, $nidn) {
     $query = ("SELECT nidn, pw, nama, id FROM adminlog WHERE nidn = ?");
