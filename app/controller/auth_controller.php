1d21ea843ba7c8becf9132b7ec1a8bb37756a4e7
sena <atharizkysena@gmail.com>
2025-11-30 22:00:43 +0700

    Update File auth_controller.php untuk Logic pada Loginpage

--- app/controller/auth_controller.php
+++ app/controller/auth_controller.php
@@ -14,21 +14,17 @@ if ($stmt->num_rows === 1) {
     $stmt->bind_result($db_nidn, $db_pw, $db_nama, $db_id);
     $stmt->fetch();
 
-    // Logika Jika Password Cocok
+    // Braching
     if ($password === $db_pw) {
         $_SESSION['nidn'] = $db_nidn;
-        $_SESSION['nama'] = $db_nama;
+        $_SESSION['nama'] = "<b>" . $db_nama . "</b>";
         $_SESSION['user_id'] = $db_id;
 
         $_SESSION['login_success'] = "Selamat Datang " . $_SESSION['nama'] . "!";
         header("Location: ../../public/admin/penerbitan.php");
         exit();
-    } else {
-        $_SESSION['login_error'] = "NIDN atau Password Salah.";
-        header("Location: ../../public/admin/loginpage.php");
-        exit();
     }
-
+    
 } else {
     $_SESSION['login_error'] = "NIDN atau Password Salah.";
     header("Location: ../../public/admin/loginpage.php");
