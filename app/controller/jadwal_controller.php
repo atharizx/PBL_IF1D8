382a7f9fe08206f12a5e77483e943a9f56417efa
sena <atharizkysena@gmail.com>
2025-12-29 21:38:26 +0700

    update file jadwal_controller.php

--- app/controller/jadwal_controller.php
+++ app/controller/jadwal_controller.php
@@ -1,53 +1,71 @@
 <?php
 session_start();
-require_once '../config/db_conn.php';
-require_once '../helper/upload_helper.php';
-
+include '../config/db_conn.php';
+include '../models/insertData_model.php';
 
 if (!isset($_POST['submitJadwal'])) {
     header("Location: ../../public/admin/penerbitan.php");
     exit();
 }
 
+$user_id = $_SESSION['user_id'];
+
+// Mengambil Data yang di input oleh user
+// Text
 $judulJadwal   = $_POST['judulJadwal'] ?? '';
 $deskripsiJadwal = $_POST['deskripsiJadwal'] ?? '';
 $masaBerlakuJadwal = $_POST['masaBerlakuJadwal'] ?? '';
 $jurusanJadwal = $_POST['jurusanJadwal'] ?? '';
+
+// File
 $fotoJadwal   = $_FILES['gambarJadwal'] ?? null;
 $excelJadwal  = $_FILES['excelJadwal'];
 
+// Variable Pembantu untu output Validasi
 $errorJadwal = [];
 $errorSizeJadwal = [];
 $errorTypeJadwal = [];
-$maxSizeJadwal = 5 * 1024 * 1024;
 $helperAlertJadwal = "tidak boleh kosong! Mohon isi input tersebut";
 $helperAlertSizeJadwal = "Melebihi Batas Ukuran";
 $helperAlertTypeJadwal = "Jenis file tidak dapat diterima";
-$allowedImgJadwal = ['png', 'jpeg', 'jpg'];
-$allowedExlJadwal = ['xls', 'xlsx'];
 
-// VALIDASI TEKS
+// Variable Pembantu Untuk Validasi File jika type tidak dikenal
+$strImgName = $fotoJadwal['name'];
+$strExcelName = $excelJadwal['name'];
+$isUploadImg = $fotoJadwal && $fotoJadwal['error'] === 0;
+$xImg = explode('.', $fotoJadwal['name'] ?? '');
+$xExcel = explode('.', $excelJadwal['name']);
+$extImg = strtolower(end($xImg));
+$extExcel = strtolower(end($xExcel));
+
+
+// Ketentuan Penyimpanan File
+$maxSizeImgJadwal = 2000000;
+$maxSizeExlJadwal = 3000000;
+$allowedImgJadwal = array('png', 'jpeg', 'jpg');
+$allowedExlJadwal = array('xls', 'xlsx');
+
+// Mengisi Data pada Array Variable Pembantu
 if (empty(trim($judulJadwal))) $errorJadwal[] = "<b>Judul </b>";
 if (empty(trim($deskripsiJadwal)))    $errorJadwal[] = "<b>Deskripsi </b>";
 if (empty(trim($masaBerlakuJadwal)))  $errorJadwal[] = "<b>Masa Berlaku </b>";
 if (empty(trim($jurusanJadwal))) $errorJadwal[] = "<b>Jurusan </b>";
 
 // VALIDASI FILE
+// Excel
 if ($excelJadwal['error'] === 4) $errorJadwal[] = "<b>File Excel</b>";
-
-$extExcel = strtolower(pathinfo($excelJadwal['name'], PATHINFO_EXTENSION));
+if ($excelJadwal['size'] > $maxSizeExlJadwal) $errorSizeJadwal[] = "<b>File Excel </b>";
 if (!in_array($extExcel, $allowedExlJadwal)) $errorTypeJadwal[] = "<b>File Excel </b>";
-if ($excelJadwal['size'] > $maxSizeJadwal) $errorSizeJadwal[] = "<b>File Excel </b>";
+move_uploaded_file($excelJadwal['tmp_name'], '../../public/upload/jadwalUjian/excel/' . $strExcelName);
 
-
-$isUploadImg = $fotoJadwal && $fotoJadwal['error'] === 0;
+// IMG
 if ($isUploadImg) {
-    if ($fotoJadwal['size'] > $maxSizeJadwal) $errorSizeJadwal[] = "<b>Foto Jadwal </b>";
-    $extImg = strtolower(pathinfo($fotoJadwal['name'], PATHINFO_EXTENSION));
-    if (!in_array($extImg, $allowedImgJadwal)) $errorTypeJadwal[] = "<b>Foto Jadwal </b>";
+    if ($fotoJadwal['size'] > $maxSizeImgJadwal) $errorSizeJadwal[] = "<b>Foto Jadwal </b>";
+    if (!in_array($extImg, $allowedImgJadwal)) $errorTypeJadwal[] = "<b>Foto Jadwal </b>" ?? null;
+    move_uploaded_file($fotoJadwal['tmp_name'], '../../public/upload/jadwalUjian/img/' . $strImgName);
 }
 
-// Respond Semua Error
+// MAIN Branching
 if (!empty($errorJadwal)) {
     $_SESSION['msg_empty_jadwal'] = "Input " . implode(", ", $errorJadwal) . " " . $helperAlertJadwal;
     header("Location: ../../public/admin/penerbitan.php");
@@ -64,38 +82,26 @@ if ($errorTypeJadwal) {
     exit();
 }
 
-// Proses Mengganti Nama File
-
-// File Excel
-$excelJadwalNewName = uploadFile($excelJadwal, "../../public/upload/jadwalUjian/excel/", "ExceljadwalUjian");
-
-// File Foto
-if ($isUploadImg) {
-    $fotoJadwalNewName = uploadFile($fotoJadwal, "../../public/upload/jadwalUjian/img/", "gambarJadwal");
-} else {
-    $fotoJadwalNewName = null;
-}
-
-require_once '../models/upload_model.php';
-
 $data = [
-    'judul'        => $judulJadwal,
-    'deskripsi'    => $deskripsiJadwal,
-    'masaberlaku'  => $masaBerlakuJadwal,
-    'jurusan'      => $jurusanJadwal,
-    'fotojadwal'   => $fotoJadwalNewName,
-    'exceljadwal'  => $excelJadwalNewName,
+    'judul' => $judulJadwal, 
+    'masaberlaku' => $masaBerlakuJadwal, 
+    'deskripsi' => $deskripsiJadwal,
+    'jurusan' => $jurusanJadwal, 
+    'exceljadwal' => $strExcelName, 
+    'fotojadwal' => $strImgName,
+    'user_id' => $user_id
 ];
 
-$insertJadwal = storeData($conn, "jadwalujian", $data);
+$result = insertDataInformation($conn, $data, 'jadwalujian');
 
-if ($insertJadwal) {
-    $_SESSION['msg_success_jadwal'] = "Informasi jadwal Ujian Berhasil Ditambahkan.";
+if ($result) {
+    $_SESSION['msg_success_jadwal'] = "Informasi Jadwal Ujian berhasil ditambahkan.";
 } else {
-    $_SESSION['msg_error_jadwal'] = "Gagal Menambahkan Informasi.";
+    $_SESSION['msg_error_jadwal'] = "Gagal menyimpan data., Harap mencoba lagi setelah beberapa menit.";
 }
 
 header("Location: ../../public/admin/penerbitan.php");
 exit();
 
-$conn->close();
+mysqli_close($conn);
+?>
