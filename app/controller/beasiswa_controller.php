62dd62e52d0e8b86d7bddbae2ff7f5d6ca84ac0b
sena <atharizkysena@gmail.com>
2025-12-29 21:37:44 +0700

    Update beasiswa_controller.php

--- app/controller/beasiswa_controller.php
+++ app/controller/beasiswa_controller.php
@@ -1,28 +1,39 @@
 <?php
 session_start();
-require_once '../config/db_conn.php';
-require_once '../helper/upload_helper.php';
-
+include '../config/db_conn.php';
 
 if (!isset($_POST['submitBeasiswa'])) {
     header("Location: ../../public/admin/penerbitan.php");
     exit();
 }
 
+$user_id = $_SESSION['user_id'];
+
+// Mengambil Data yang di input oleh user
+// Text
 $namaBeasiswa    = $_POST['namaBeasiswa'] ?? '';
 $deskripsiBeasiswa = $_POST['deskripsiBeasiswa'] ?? '';
 $masaBerlakuBeasiswa = $_POST['masaBerlakuBeasiswa'] ?? '';
 $linkpendaftaran = $_POST['linkBeasiswa'] ?? '';
+
+// File
 $fotoBeasiswa = $_FILES['gambarBeasiswa'] ?? null;
 
+// Variable Pembantu Untuk Output Validasi
 $errorBeasiswa = [];
-$errorSizeBeasiswa = [];
-$errorTypeBeasiswa = [];
-$maxSizeBeasiswa = 5 * 1024 * 1024;
 $helperAlertBeasiswa = "tidak boleh kosong! Mohon isi input tersebut";
 $helperAlertSizeBeasiswa = "Melebihi Batas Ukuran";
 $helperAlertTypeBeasiswa = "Jenis file tidak dapat diterima";
-$allowedImgBeasiswa = ['png','jpeg','jpg'];
+
+// Variable Pembantu Untuk Validasi File
+$strImgName = $fotoBeasiswa['name'];
+$isUploadImg = $fotoBeasiswa && $fotoBeasiswa['error'] === 0;
+$xFoto = explode('.', $fotoBeasiswa['name']);
+$extImg = strtolower(end($xFoto));
+
+// Ketentuan Penyimpanan File Img
+$maxSizeImgBeasiswa = 2000000;
+$allowedImgBeasiswa = array('png', 'jpeg', 'jpg');
 
 // VALIDASI TEKS
 if (empty(trim($namaBeasiswa))) $errorBeasiswa[] = "<b>Judul</b>";
@@ -31,14 +42,13 @@ if (empty(trim($masaBerlakuBeasiswa)))  $errorBeasiswa[] = "<b>Masa Berlaku</b>"
 if (empty(trim($linkpendaftaran))) $errorBeasiswa[] = "<b>Link Pendaftaran</b>";
 
 // VALIDASI FILE
-$isUploadImg = $fotoBeasiswa && $fotoBeasiswa['error'] === 0;
 if ($isUploadImg) {
-    if ($fotoBeasiswa['size'] > $maxSizeBeasiswa) $errorSizeBeasiswa[] = "<b>Foto Beasiswa</b>";
-    $extImg = strtolower(pathinfo($fotoBeasiswa['name'], PATHINFO_EXTENSION));
-    if (!in_array($extImg, $allowedImgBeasiswa)) $errorTypeBeasiswa[] = "<b>Foto Beasiswa</b>";
+    if ($fotoBeasiswa['size'] > $maxSizeImgBeasiswa) $errorSizeBeasiswa[] = "<b>Foto Beasiswa </b>";
+    if (!in_array($extImg, $allowedImgBeasiswa)) $errorTypeBeasiswa[] = "<b>Foto Beasiswa </b>";
+    move_uploaded_file($fotoBeasiswa['tmp_name'], '../../public/upload/beasiswa/img/' . $strImgName);
 }
 
-// Respond Session Error
+// Main Branching
 if (!empty($errorBeasiswa)) {
     $_SESSION['msg_empty_beasiswa'] = "Input " . implode(", ", $errorBeasiswa) . " " . $helperAlertBeasiswa;
     header("Location: ../../public/admin/penerbitan.php");
@@ -55,34 +65,24 @@ if ($errorTypeBeasiswa) {
     exit();
 }
 
-// Proses Penggantian Nama File
-
-// File Foto (Jika Ada)
-if ($isUploadImg) {
-    $fotoBeasiswaNewName = uploadFile($fotoBeasiswa, "../../public/upload/beasiswa/img/", "gambarBeasiswa");
-} else {
-    $fotoBeasiswaNewName = null;
-}
-
-require_once '../models/upload_model.php';
-
 $data = [
-    'namabeasiswa' => $namaBeasiswa,
-    'deskripsi'    => $deskripsiBeasiswa,
-    'masaberlaku'  => $masaBerlakuBeasiswa,
-    'linkpendaftaran' => $linkpendaftaran,
-    'fotobeasiswa' => $fotoBeasiswaNewName,
+    'namabeasiswa' => $namaBeasiswa, 
+    'masaberlaku' => $masaBerlakuBeasiswa, 
+    'deskripsi' => $deskripsiBeasiswa, 
+    'linkpendaftaran' => $linkpendaftaran, 
+    'fotobeasiswa' => $strImgName, 
+    'user_id' => $user_id
 ];
 
-$insertBeasiswa = storeData($conn, "beasiswa", $data);
+$result = insertDataInformation($conn, $query, 'beasiswa');
 
-if ($insertBeasiswa) {
-    $_SESSION['msg_success_beasiswa'] = "Informasi Beasiswa Berhasil Ditambahkan.";
+if ($result) {
+    $_SESSION['msg_success_beasiswa'] = "Informasi Beasiswa berhasil ditambahkan.";
 } else {
-    $_SESSION['msg_error_beasiswa'] = "Gagal Menambahkan Informasi.";
+    $_SESSION['msg_error_beasiswa'] = "Gagal menambahkan informasi, Harap mencoba lagi setelah beberapa menit.";
 }
 
 header("Location: ../../public/admin/penerbitan.php");
 exit();
 
-$conn->close();
+mysqli_close($conn);
