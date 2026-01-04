e889ce3818c3e1ad26420cf8fd7c5ac19da29155
zera <tenrinarang@gmail.com>
2026-01-04 20:11:31 +0700

    Update file usercard_model.php
    menambah filter dalam pemanggilan data,Menambahkan function baru untuk pemanggilan data untuk searchbar pada Halaman beranda

--- app/models/usercard_model.php
+++ app/models/usercard_model.php
@@ -1,17 +1,37 @@
 <?php
 
-function takeData($conn, $table, $data) {
-    $column = implode(", ", $data);
-
-    $query = "SELECT $column FROM $table ORDER BY id DESC";
-    $stmt = $conn->prepare($query);
-    $stmt->execute();
-    $result = $stmt->get_result();
-
+// Function Untuk Mengambil Data dari database berdasarkan tabel, data yang diperlukan, dan filter tertentu
+function takeData($conn, $table, $data, $filter) {
+    // Menyiapkan query untuk mengambil data
+    $query = "SELECT $data FROM $table WHERE $filter ORDER BY id DESC";
+    
+    // Eksekusi query
+    $queryRun = mysqli_query($conn, $query);
     $rows = [];
-    while ($row = $result->fetch_assoc()) {
-        $rows[] = $row;
+    
+    // Mengambil data dari hasil eksekusi query
+    if ($queryRun) {
+        while ($row = mysqli_fetch_assoc($queryRun)) {
+            $rows[] = $row;
+        }
     }
+    
+    return $rows;
+}
 
-    return $rows; 
+// Function Untuk mengambil data berdasarkan pencarian yang di input user
+function searchDataHandler($conn, $table, $data, $value) {
+    // menyiapkan query untuk mengambil data berdasarkan pencarian
+    $query = "SELECT $data FROM $table WHERE $value";
+    // Eksekusi query
+    $queryRun = mysqli_query($conn, $query);
+
+    // Mengambil data dari hasil eksekusi query
+    $result = [];
+    if ($queryRun) {
+        while ($row = mysqli_fetch_assoc($queryRun)) {
+            $result[] = $row;
+        }
+    }
+    return $result;
 }
